e3bfe59aef91fc3522b0f2d99e37d7cb09f79b24
Nikolai Titenok <titenok.n@yandex.ru>
2024-03-03 14:58:33 +0300

    Починить путь к файлу resource

--- tests/test_registration_page.py
+++ tests/test_registration_page.py
@@ -6,9 +6,3 @@ def test_student_registration():
     app.student_registration.open_form()
     app.student_registration.fill_student_form(users.nikolai)
     app.student_registration.assert_student_form(users.nikolai)
-
-
-    # def test_text_box(self):
-    #     app.left_panel.open_text_box()
-    #     app.text_box.fill_user_form(self.nikolai)
-    #     app.text_box.assert_user_form(self.nikolai)
